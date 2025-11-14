c0a3426934f1f4f0baf27e2426bd327f41d9ccfd
xameryn <maximilianramann@gmail.com>
2025-11-14 03:11:42 -0800

    Make Supplementaries sign button adjust platform agnostic

--- src/main/java/net/rasanovum/viaromana/mixins/client/TextHolderEditScreenMixin.java
+++ src/main/java/net/rasanovum/viaromana/mixins/client/TextHolderEditScreenMixin.java
@@ -2,22 +2,16 @@ package net.rasanovum.viaromana.mixins.client;
 
 import net.mehvahdjukaar.supplementaries.client.screens.SignPostScreen;
 import net.mehvahdjukaar.supplementaries.client.screens.TextHolderEditScreen;
-import net.minecraft.client.gui.screens.Screen;
-import net.rasanovum.viaromana.util.SignEditHelper;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-//? if fabric {
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.Version;
-import net.fabricmc.loader.api.VersionParsingException;
-//?}
+import net.rasanovum.viaromana.util.SignEditHelper;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(TextHolderEditScreen.class)
 public abstract class TextHolderEditScreenMixin extends Screen {
@@ -26,38 +20,27 @@ public abstract class TextHolderEditScreenMixin extends Screen {
         super(title);
     }
 
-    @Inject(method = "init()V", at = @At("TAIL"))
-    private void addLinkButton(CallbackInfo ci) {
+    @Inject(method = "init", at = @At("TAIL"))
+    private void signPostScreenInit(CallbackInfo ci) {
         if ((Object) this instanceof SignPostScreen) {
             Button linkButton = SignEditHelper.createLinkButton((SignPostScreen) (Object) this);
-            if (linkButton != null) this.addRenderableWidget(linkButton);
+            if (linkButton != null) {
+                this.addRenderableWidget(linkButton);
+            }
+        }
+
+        GuiEventListener buttonWidget = this.children().get(0);
+
+        if (buttonWidget instanceof Button doneButton) {
+            int vanillaY = this.height / 4 + 144;
+            if (doneButton.getY() != vanillaY) {
+                doneButton.setY(vanillaY);
+            }
         }
     }
 
-    // Fixed in Supplementaries 3.1.38
-    //? if fabric {
-    // Match vanilla abstract sign positioning
-    @ModifyArgs(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/Button$Builder;bounds(IIII)Lnet/minecraft/client/gui/components/Button$Builder;"))
-    private void adjustDoneButtonPosition(Args args) {
-        try {
-            Version fixVersion = Version.parse("1.20-3.1.38");
-
-            FabricLoader.getInstance().getModContainer("supplementaries").ifPresent(modContainer -> {
-                Version installedVersion = modContainer.getMetadata().getVersion();
-
-                if (installedVersion.compareTo(fixVersion) < 0) {
-                    int originalY = args.get(1);
-                    args.set(1, originalY + 24);
-                }
-            });
-        } catch (VersionParsingException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
-    //?}
-
-    // Disables pausing when editing signpost
-    // public boolean isPauseScreen() {
-    //     return false;
-    // }
 }
